fixed the impedence control and the forward kinematics and jacobian calculation

Robot Autonomy/16_662_HW1/Controls/PandaArmControl_Part1.py
@@ -78,6 +78,10 @@ def impedance_control(model, data): #TODO:
     2. This means it's not suitable for position control but can only exert forces
     3. Therefore the only output we command is how much force our robot will exert
     4. However, here we construct our wrench differently as = stiffness*pos_error + damping*vel_error
+    5. Impedence control allows for compliant movement, where motion is not restrictive in one
+       direction but can be made restrictive in other direction. We can also try this out
+       ourselves by making Kp a diagonal matrix and making the gains high for all other
+       directions other than x-axis and then double clicking the robot in simulator to move it
     """
 
     # Implement an impedance control callback here that generates a force of 15 N along the global x-axis,
@@ -93,9 +97,7 @@ def impedance_control(model, data): #TODO:
     The overall position will be 1x6 vector and so will velocity (positional + angular)
     The position is a 1x6 vector because it contains [x,y,z,roll,pitch,yaw]
 
-    However for impedence control we don't care about angular aspect of position
-
-    !NOTE: We have implemented a hacky version of impedence control for PandaArmControl_Part2
+    However we don't care about angular aspect of position in this question
     """
 
     # Instantite a handle to the desired body on the robot
@@ -104,8 +106,10 @@ def impedance_control(model, data): #TODO:
     id = mj.mj_name2id(model, mj.mjtObj.mjOBJ_BODY, "hand")
 
     # Set the desired position (we'll set it to a 1x3 matrix now,
-    #                           but we'll combine with rotations later to make it a 1x6)
-    pos_des = np.array([0.595, 0, 0.595])
+    #                           but we'll combine with rotations (zero vector here) later to make it a 1x6)
+    # Also, we are appending 1 to the desired position because even after we reach the board
+    # i.e. steady_state, we will have to 
+    pos_des = np.array([0.595 + 1, 0, 0.595])
 
     # Set the desired velocities (positional vel only therfore 3x1)
     vel_des = np.zeros((3,1))
@@ -132,6 +136,8 @@ def impedance_control(model, data): #TODO:
     # orientation_error_blank = np.array([0,0,0])
 
     # Get the errors
+    # since vel_des is zero, and current vel will be high due to error in position
+    # this vel_error will be negative and will act as a damper on the whole system
     vel_error = vel_des - vel_curr
     # expanding vel_error to 6x1
     vel_error_final = np.zeros((6,1))
@@ -145,7 +151,7 @@ def impedance_control(model, data): #TODO:
     print("vel error final shape", vel_error_final.shape)
 
     # Compute the impedance control input torques
-    stiffness = 31
+    stiffness = 15
     damping = 20
 
     # Get the Jacobian at the desired location on the robot
@@ -160,14 +166,10 @@ def impedance_control(model, data): #TODO:
     # get the final jacobian by combining jacp and jacr
     final_jacobian = np.concatenate((jacp, jacr)) # will be 6xmodel.nv shape
 
-    # This function works by taking in return parameters!!! Make sure you supply it with placeholder
-    # variables
-
     # Set the control inputs (specific to impedence control)
     required_ctrl_inp = np.squeeze(final_jacobian.T @ (damping*vel_error_final + stiffness*pos_error_final))
 
     data.ctrl[:7] = data.qfrc_bias[:7] + required_ctrl_inp
-
 
     # DO NOT CHANGE ANY THING BELOW THIS IN THIS FUNCTION
 
@@ -216,7 +218,7 @@ if __name__ == "__main__":
     # compensation callback has been implemented for you. Run the file and play with the model as
     # explained in the PDF
 
-    mj.set_mjcb_control(impedance_control) #TODO:
+    mj.set_mjcb_control(force_control) #TODO:
 
     ################################# Swap Callback Above This Line #################################
 
@@ -229,7 +231,7 @@ if __name__ == "__main__":
     viewer.launch(model, data)
 
     # Save recorded force and time points as a csv file
-    # force = np.reshape(force, (5000, 1))
-    # time = np.reshape(time, (5000, 1))
-    # plot = np.concatenate((time, force), axis=1)
-    # np.savetxt('force_vs_time.csv', plot, delimiter=',')
+    force = np.reshape(force, (5000, 1))
+    time = np.reshape(time, (5000, 1))
+    plot = np.concatenate((time, force), axis=1)
+    np.savetxt('force_vs_time.csv', plot, delimiter=',')

Robot Autonomy/16_662_HW1/Controls/PandaArmControl_Part2.py
@@ -89,17 +89,17 @@ def force_control(model, data):
 
 # Control callback for an impedance controller
 def impedance_control(model, data):
-
-    # Copy the code that you used to implement the controller from Part1 here
     """
     Impedence Control Basics:
 
     1. Impedecne controller are interaction controller
     2. This means it's not suitable for position control but can only exert forces
     3. Therefore the only output we command is how much force our robot will exert
-    4. However, since we're not giving a wrench like force control we need to tune our Kp and errors instead
-    5. However, since we know we only need a force of 15N along x-axis, we only need to tune
-       the error such that error*Kp = 15N. Easy solution is set Kp=100 and set error = 15/Kp
+    4. However, here we construct our wrench differently as = stiffness*pos_error + damping*vel_error
+    5. Impedence control allows for compliant movement, where motion is not restrictive in one
+       direction but can be made restrictive in other direction. We can also try this out
+       ourselves by making Kp a diagonal matrix and making the gains high for all other
+       directions other than x-axis and then double clicking the robot in simulator to move it
     """
 
     # Implement an impedance control callback here that generates a force of 15 N along the global x-axis,
@@ -115,8 +115,7 @@ def impedance_control(model, data):
     The overall position will be 1x6 vector and so will velocity (positional + angular)
     The position is a 1x6 vector because it contains [x,y,z,roll,pitch,yaw]
 
-    Now, the simulator works on quaternions, but for our math of Jacobian transpose we need
-    roll,pitch,yaw. Therefore we convert the quaternion error to roll,pitch,yaw error finally
+    However we don't care about angular aspect of position in this question
     """
 
     # Instantite a handle to the desired body on the robot
@@ -125,12 +124,13 @@ def impedance_control(model, data):
     id = mj.mj_name2id(model, mj.mjtObj.mjOBJ_BODY, "hand")
 
     # Set the desired position (we'll set it to a 1x3 matrix now,
-    #                           but we'll combine with rotations later to make it a 1x6)
-    #!ASK TA WHAT THIS SHOULD BE
-    pos_des = np.array([0.595, 0, 0.595])
+    #                           but we'll combine with rotations (zero vector here) later to make it a 1x6)
+    # Also, we are appending 1 to the desired position because even after we reach the board
+    # i.e. steady_state, we will have to
+    pos_des = np.array([0.595 + 1, 0, 0.595])
 
-    # Set the desired velocities (positional and anglur is a 1x6 array)
-    vel_des = np.zeros((6,1))
+    # Set the desired velocities (positional vel only therfore 3x1)
+    vel_des = np.zeros((3,1))
 
     # Set the desired orientation (Use numpy quaternion manipulation functions)
     # convert desired_(roll,pitch,yaw) to desired_quaternion
@@ -141,26 +141,36 @@ def impedance_control(model, data):
     pos_curr = end_effector.xpos
     vel_curr = np.zeros((6,1))
     mj.mj_objectVelocity(model, data, mj.mjtObj.mjOBJ_BODY, id, vel_curr, True)
+    # for impedence control we need only the first 3 elements
+    vel_curr = vel_curr[0:3,:]
+    print(vel_curr.shape)
 
-    # Get orientation error
-    orientation_error = orientation_des - orientation_curr
-    orientation_error = quaternion.from_float_array(orientation_error)
-    orientation_error_axis_angle = quaternion.as_rotation_vector(orientation_error)
+    # Get orientation error (Not using this for impedence control)
+    # orientation_error = orientation_des - orientation_curr
+    # print("orientation error in quaternion", orientation_error)
+    # orientation_error = quaternion.from_float_array(orientation_error)
+    # orientation_error_axis_angle = quaternion.as_rotation_vector(orientation_error)
+    # print("orientation error in axis angle", orientation_error_axis_angle)
+    # orientation_error_blank = np.array([0,0,0])
 
     # Get the errors
+    # since vel_des is zero, and current vel will be high due to error in position
+    # this vel_error will be negative and will act as a damper on the whole system
     vel_error = vel_des - vel_curr
+    # expanding vel_error to 6x1
+    vel_error_final = np.zeros((6,1))
+    vel_error_final[0:3,0] = vel_error.squeeze()
     pos_error = pos_des - pos_curr
-    pos_error = np.concatenate([pos_error, orientation_error_axis_angle])
-    pos_error = np.expand_dims(pos_error, axis=1)
+    # expanding pos_error to 6x1
+    pos_error_final = np.zeros((6,1))
+    pos_error_final[0:3,0] = pos_error.squeeze()
+
+    print("pos error final shape", pos_error_final.shape)
+    print("vel error final shape", vel_error_final.shape)
 
     # Compute the impedance control input torques
-    Kp = 2
-    Kd = 2
-
-    # Now everthing we did above for position error was for something more like a
-    # position control, however, for impedence control in this case we need only along x-axis
-    pos_error_new = np.zeros((6,1), dtype=np.float32)
-    pos_error_new[0] = 15/Kp
+    stiffness = 15
+    damping = 20
 
     # Get the Jacobian at the desired location on the robot
     # init a dummy jacobian matrix of the right size (model.nv gives the DOF required)
@@ -177,11 +187,8 @@ def impedance_control(model, data):
     # get the final jacobian by combining jacp and jacr
     final_jacobian = np.concatenate((jacp, jacr)) # will be 6xmodel.nv shape
 
-    # This function works by taking in return parameters!!! Make sure you supply it with placeholder
-    # variables
-
-    # Set the control inputs
-    required_ctrl_inp = np.squeeze(final_jacobian.T @ (Kd*vel_error + Kp*pos_error_new))
+    # Set the control inputs (specific to impedence control)
+    required_ctrl_inp = np.squeeze(final_jacobian.T @ (damping*vel_error_final + stiffness*pos_error_final))
 
     data.ctrl[:7] = data.qfrc_bias[:7] + required_ctrl_inp
 
@@ -215,11 +222,6 @@ def position_control(model, data):
     data.ctrl[:7] = data.qfrc_bias[:7] + Kp*(desired_joint_positions-data.qpos[:7]) + Kd*(np.array([0,0,0,0,0,0,0])-data.qvel[:7])
 
 
-
-
-
-
-
 ####################################### MAIN #####################################
 
 
@@ -239,7 +241,7 @@ if __name__ == "__main__":
     # compensation callback has been implemented for you. Run the file and play with the model as
     # explained in the PDF
 
-    mj.set_mjcb_control(impedance_control) #TODO:
+    mj.set_mjcb_control(force_control) #TODO:
 
     ################################# Swap Callback Above This Line #################################
 

Robot Autonomy/16_662_HW1/Kinematics/Franka.py
@@ -1,12 +1,13 @@
 import numpy as np
 import RobotUtil as rt
 import math
+import sys
 # import matplotlib.pyplot as plt
 # from mpl_toolkits.mplot3d import Axes3D
 
 
 class FrankArm:
-    
+
     def __init__(self):
 		# Robot descriptor taken from URDF file (rpy xyz for each rigid link transform) - NOTE: don't change
         self.Rdesc=[
@@ -19,8 +20,8 @@ class FrankArm:
             [np.pi/2, 0, 0, 0.088, 0, 0],
             [0, 0, 0, 0, 0, 0.107] # From joint5 to end-effector center
 			]
-        
-		#Define the axis of rotation for each joint 
+
+		#Define the axis of rotation for each joint
         self.axis=[
 				[0, 0, 1],
 				[0, 0, 1],
@@ -37,53 +38,77 @@ class FrankArm:
 			[0,1,0,0],
 			[0,0,1,0],
 			[0,0,0,1]]
-		
+
 		#Initialize matrices - NOTE: don't change this part
-        self.Tlink=[] #Transforms for each link (const)
-        self.Tjoint=[] #Transforms for each joint (init eye)
-        self.Tcurr=[] #Coordinate frame of current (init eye)
+        self.Tlink=[] # Transforms for each link (const)
+        self.Tjoint=[] # Transforms for each joint (init eye)
+        self.Tcurr=[] # Coordinate frame of current (init eye)
         for i in range(len(self.Rdesc)):
             self.Tlink.append(rt.rpyxyz2H(self.Rdesc[i][0:3],self.Rdesc[i][3:6]))
             self.Tcurr.append([[1,0,0,0],[0,1,0,0],[0,0,1,0.],[0,0,0,1]])
             self.Tjoint.append([[1,0,0,0],[0,1,0,0],[0,0,1,0.],[0,0,0,1]])
 
-        self.Tlinkzero=rt.rpyxyz2H(self.Rdesc[0][0:3],self.Rdesc[0][3:6])  
+        self.Tlinkzero=rt.rpyxyz2H(self.Rdesc[0][0:3],self.Rdesc[0][3:6])
 
-        self.Tlink[0]=np.matmul(self.Tbase,self.Tlink[0])					
+        self.Tlink[0]=np.matmul(self.Tbase,self.Tlink[0])
 
 		# initialize Jacobian matrix
         self.J=np.zeros((6,7))
-		
+
         self.q=[0.,0.,0.,0.,0.,0.,0.]
         self.ForwardKin([0.,0.,0.,0.,0.,0.,0.])
-        
+
     def ForwardKin(self,ang):
         '''
-		inputs: joint angles
+		inputs: target joint angles
 		outputs: joint transforms for each joint, Jacobian matrix
 		'''
-        
-        self.q[0:-1]=ang
-		
-		#Compute current joint and end effector coordinate frames (self.Tjoint). Remember that not all joints rotate about the z axis!
-        
-            
+
+        # input angles
+        self.q[0:-1] = ang
+
+		# Compute current joint and end effector coordinate frames (self.Tjoint). Remember that not all joints rotate about the z axis!
+        # we need to use the input angles and transform each joint to finally get the end effector pose
+        for i in range(len(self.q)):
+            # initially self.Tjoint would be at the home position of each joint defined above
+            self.Tjoint[i] = [
+                                [math.cos(self.q[i]), -math.sin(self.q[i]), 0, 0],
+                                [math.sin(self.q[i]), math.cos(self.q[i]), 0, 0],
+                                [0,0,1,0],
+                                [0,0,0,1],
+                             ]
+
+            if i == 0:
+                self.Tcurr[i] = np.matmul(self.Tlink[i], self.Tjoint[i])
+            else:
+                # compute the i'th joint's frame by chaining previous frame && link_transform && join_transform
+                self.Tcurr[i] = np.matmul(np.matmul(self.Tcurr[i-1], self.Tlink[i]), self.Tjoint[i])
+
+        # compute the jacobian
+        # loop over all joints expect the last one. Tcurr is the transformation matrix of each joint
+        for i in range(len(self.Tcurr)-1):
+            #p = compare translation of last joint with ith joint
+            p = self.Tcurr[-1][0:3,3] - self.Tcurr[i][0:3,3]
+            a = self.Tcurr[i][0:3,2] # [0:3,2] = z-axis of i'th joint
+            # a = angular vel of the revolute joint and p = position vector from joint i -> end effector
+            # cross product of the two gives that column of the jacobian
+            self.J[0:3,i] = np.cross(a,p)
+            self.J[3:7,i] = a
+
         return self.Tcurr, self.J
 
-		
 
     def IterInvKin(self,ang,TGoal,x_eps=1e-3, r_eps=1e-3):
         '''
 		inputs: starting joint angles (ang), target end effector pose (TGoal)
 
-		outputs: computed joint angles to achieve desired end effector pose, 
+		outputs: computed joint angles to achieve desired end effector pose,
 		Error in your IK solution compared to the desired target
-		'''	
+		'''
 
         W = np.eye(7)
         C = np.eye(6)
 
         Err = 0
-		
 
         return self.q[0:-1], Err

Robot Autonomy/16_662_HW1/Kinematics/MainTest.py
@@ -15,7 +15,7 @@ joint_targets = [[0., 0., 0., 0., 0., 0., 0.], \
 for joint_target in joint_targets:
   print ('\nJoints:')
   print (joint_target)
-  Hcurr,J =mybot.ForwardKin(joint_target)
+  Hcurr, J = mybot.ForwardKin(joint_target)
   ee_pose = Hcurr[-1]
   rot_ee = ee_pose[:3,:3]
   pos_ee = ee_pose[:3,3]
@@ -32,6 +32,6 @@ HGoal= np.array([[0.,0.,1.,0.6], # target EE pose
 		 [-1.,0,0.,0.5],
 		 [0.,0.,0.,1]])
 
-q,Err=mybot.IterInvKin(qInit, HGoal)
+q, Err = mybot.IterInvKin(qInit, HGoal)
 print('Error', np.linalg.norm(Err[0:3]),np.linalg.norm(Err[3:6]))
 print('Computed IK angles', q)